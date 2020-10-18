74af0ccb4cc465137e6b9fc75e2e8d705e9260a6
sshsuns <dysun1024@gmail.com>
2020-10-19 00:47:58 +1100

    update postController and userController

---
+++ server/controllers/postController.js
@@ -0,0 +1,49 @@
+var Post = require('../models/post');
+
+const { body, validationResult } = require("express-validator");
+
+// Go to page for publish an event.
+exports.post_create_g = function (req, res, next) {
+
+};
+
+// Handle Post create on POST.
+exports.post_create_p = function (req, res, next) {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create User object with escaped and trimmed data
+    var post = new Post(
+        {
+            publisher_user: req.body.publisher_user,
+            quest_type: req.body.quest_type,
+            quest_discription: req.body.quest_discription,
+            post_date: req.body.post_date,
+            due_date: req.body.due_date,
+            acceptant_user: req.body.acceptant_user,
+            post_state: req.body.post_state,
+        }
+    );
+
+    if (!errors.isEmpty()) {
+        // There are errors. Render form again with sanitized values/errors messages.
+    }
+    else {
+        // Data from form is valid.
+        // Save Post.
+        user.save(function (err) {
+            if (err) { return next(err); }
+            // Successful - redirect to new user record.          
+            res.status(201).json({
+                success: true,
+                post
+            });
+        });
+    }
+};
+
+
+
+
+
+
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,31 +1,55 @@
 var User = require('../models/user');
 
-const { body,validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 
-// Display detail page for a specific Author.
-exports.author_detail = function (req, res, next) {
-
-    async.parallel({
-        author: function (callback) {
-            Author.findById(req.params.id)
-                .exec(callback)
-        },
-        authors_books: function (callback) {
-            Book.find({ 'author': req.params.id }, 'title summary')
-                .exec(callback)
-        },
-    }, function (err, results) {
-        if (err) { return next(err); } // Error in API usage.
-        if (results.author == null) { // No results.
-            var err = new Error('Author not found');
-            err.status = 404;
-            return next(err);
-        }
-        // Successful, so render.
-        res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books });
-    });
+// Display User create form on GET.
+exports.user_create_g = function (req, res, next) {
 
 };
+
+// Handle User create on POST.
+exports.user_create_p = function (req, res, next) {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create User object with escaped and trimmed data
+    var user = new User(
+        {
+            user_name: req.body.user_name,
+            email: req.body.email,
+            password: req.body.password,
+        }
+    );
+
+    if (!errors.isEmpty()) {
+        // There are errors. Render form again with sanitized values/errors messages.
+    }
+    else {
+        // Data from form is valid.
+        // Save User.
+        user.save(function (err) {
+            if (err) { return next(err); }
+            // Successful - redirect to new user record.          
+            res.status(201).json({
+                success: true,
+                user
+            });
+        });
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Handle User create on POST.
@@ -34,7 +58,7 @@ exports.user_create_post = [
     // Validate and sanitize fields.
 
     body('user_name').trim().isLength({ min: 1 }).escape().withMessage('User name must be specified.')
-        .isAlphanumeric().withMessage('User name has non-alphanumeric characters.'),  
+        .isAlphanumeric().withMessage('User name has non-alphanumeric characters.'),
     body('email').trim().isLength({ min: 1 }).escape().withMessage('Email must be specified.'),
     body('password').trim().isLength({ min: 1 }).escape().withMessage('Email must be specified.'),
 
@@ -43,7 +67,7 @@ exports.user_create_post = [
 
         // Extract the validation errors from a request.
         const errors = validationResult(req);
-        
+
         // Create User object with escaped and trimmed data
         var user = new User(
             {
@@ -55,7 +79,7 @@ exports.user_create_post = [
 
         if (!errors.isEmpty()) {
             // There are errors. Render form again with sanitized values/errors messages.
-            
+
         }
         else {
             // Data from form is valid.
