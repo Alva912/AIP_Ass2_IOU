d01d202adef28a74096d0999f89a7e1825c1bb83
sshsuns <dysun1024@gmail.com>
2020-10-19 00:46:34 +1100

    add route for creating new post and new user

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+//Require all controllers.
+var post_controller = require('../controllers/postController');
+var reward_controller = require('../controllers/rewardController');
+var user_controller = require('../controllers/userController');
+
+// GET request for creating User. Note this must come before route for id (i.e. display user).
+router.get('/signup', user_controller.user_create_g);
+
+// POST request for creating User.
+router.post('/signup', user_controller.user_create_p);
+
+// Go to page for publish an event.
+router.get('/post', post_controller.post_create_g);
+
+//POST request for creating Post.
+router.post('/post', post_controller.post_create_p);
+
+
+
+
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+
 
 
 
