f7b74384bf2be11c04fdcdb7866972ec0383a453
sshsuns <dysun1024@gmail.com>
2020-10-19 19:02:59 +1100

    updata skeleton functions

--- server/controllers/rewardController.js
+++ server/controllers/rewardController.js
@@ -5,6 +5,47 @@ var Reward = require('../models/reward');
 var async = require('async');
 const { body, validationResult } = require("express-validator");
 
+// Display list of all rewards
+exports.reward_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: reward list');
+}
+
+// Display detail info for a specific reward.
+exports.reward_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Reward detail: ' + req.params.id);
+};
+
+// Display reward create form on GET.
+exports.reward_create_g = function(req, res) {
+    res.send('NOT IMPLEMENTED: Reward create GET');
+};
+
+// Handle reward create on POST.
+exports.reward_create_p = function(req, res) {
+    res.send('NOT IMPLEMENTED: Reward create POST');
+};
+
+// Display reward delete form on GET.
+exports.reward_delete_g = function(req, res) {
+    res.send('NOT IMPLEMENTED: Reward delete GET');
+};
+
+// Handle reward delete on POST.
+exports.reward_delete_p = function(req, res) {
+    res.send('NOT IMPLEMENTED: reward delete POST');
+};
+
+// Display reward update form on GET.
+exports.reward_update_g = function(req, res) {
+    res.send('NOT IMPLEMENTED: reward update GET');
+};
+
+// Handle reward update on POST.
+exports.reward_update_p = function(req, res) {
+    res.send('NOT IMPLEMENTED: reward update POST');
+};
+
+
 // Display User owe other
 exports.userOweOther = function (req, res, next) {
 
