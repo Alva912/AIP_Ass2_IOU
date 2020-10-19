445e6014e5dbec0629e89500119bbba61f3d32f1
sshsuns <dysun1024@gmail.com>
2020-10-20 03:47:48 +1100

    improve date format

--- server/models/post.js
+++ server/models/post.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
 
 var Schema = mongoose.Schema;
 
@@ -7,6 +8,7 @@ var PostSchema = new Schema(
     publisher_user: {type: Schema.Types.ObjectId, ref: 'User', require: true},
     quest_type: {type: String, require: true, enum: ['Cleaning', 'Snacking'], default: 'Cleaning'},
     quest_discription: {type: String, required: true, maxlength: 300, minlength: 2},
+    reward: {type: Schema.Types.ObjectId, ref: 'Reward', require: true},
     post_date: {type: Date, default: Date.now},
     due_date: {type: Date, default: Date.now},
     acceptant_user: {type: Schema.Types.ObjectId, ref: 'User'},
@@ -14,11 +16,25 @@ var PostSchema = new Schema(
   }
 );
 
-// Virtual for post's URL
+// Virtual for post's URL.
 PostSchema
 .virtual('url')
 .get(function () {
   return '/post/' + this._id;
+});
+
+// Virtual for post's post_date.
+PostSchema
+.virtual('post_date_formatted')
+.get(function () {
+return DateTime.fromJSDate(this.post_date).toLocaleString(DateTime.DATE_MED);
+});
+
+// Virtual for post's due_date.
+PostSchema
+.virtual('due_date_formatted')
+.get(function () {
+return DateTime.fromJSDate(this.due_date).toLocaleString(DateTime.DATE_MED);
 });
 
 //Export model
