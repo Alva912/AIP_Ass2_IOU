e33ea62a22c79caa5c3c915296e4e9201e30f831
sshsuns <dysun1024@gmail.com>
2020-10-19 19:01:57 +1100

    update routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,10 @@ var post_controller = require('../controllers/postController');
 var reward_controller = require('../controllers/rewardController');
 var user_controller = require('../controllers/userController');
 
+// Front page - Site Home Page
+router.get('/', post_controller.index);
+
+
 // GET request for creating User. Note this must come before route for id (i.e. display user).
 router.get('/signup', user_controller.user_create_g);
 
@@ -17,12 +21,6 @@ router.get('/post/publish', post_controller.post_create_g);
 
 //POST request for creating Post.
 router.post('/post/create', post_controller.post_create_p);
-
-
-
-
-
-
 
 
 /* GET home page. */
