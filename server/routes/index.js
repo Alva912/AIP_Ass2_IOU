0a516dbc126aa930c567f91e77b6fdc6f75d5adc
sshsuns <dysun1024@gmail.com>
2020-10-19 20:55:24 +1100

    add some kinds of routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,27 +6,84 @@ var post_controller = require('../controllers/postController');
 var reward_controller = require('../controllers/rewardController');
 var user_controller = require('../controllers/userController');
 
-// Front page - Site Home Page
+//GET front page - Site Home Page
 router.get('/', post_controller.index);
 
+// REWARD ROUTES //
+// GET request for creating a reward. NOTE This must come before routes that display reward (uses id).
+router.get('/reward/create', reward_controller.createReward_g);
 
-// GET request for creating User. Note this must come before route for id (i.e. display user).
-router.get('/signup', user_controller.user_create_g);
+// POST request for creating reward.
+router.post('/reward/create', reward_controller.createReward_p);
 
-// POST request for creating User.
-router.post('/signup', user_controller.user_create_p);
+// GET request to delete reward.
+router.get('/reward/:id/delete', reward_controller.deleteReward_g);
 
-// Go to page for publish an event.
-router.get('/post/publish', post_controller.post_create_g);
+// POST request to delete reward.
+router.post('/reward/:id/delete', reward_controller.deleteReward_p);
 
-//POST request for creating Post.
-router.post('/post/create', post_controller.post_create_p);
+// GET request to update reward.
+router.get('/reward/:id/update', reward_controller.updateReward_g);
+
+// POST request to update reward.
+router.post('/reward/:id/update', reward_controller.updateReward_p);
+
+// GET request for one reward.
+router.get('/reward/:id', reward_controller.getRewardById);
+
+// GET request for list of all reward.
+router.get('/rewards', reward_controller.getAllRewards);
+
+// USER ROUTES //
+// GET request for creating user. NOTE This must come before route for id (i.e. display user).
+router.get('/user/create', user_controller.createUser_g);
+
+// POST request for creating user.
+router.post('/user/create', user_controller.createUser_p);
+
+// GET request to delete user.
+router.get('/user/:id/delete', user_controller.deleteUser_g);
+
+// POST request to delete user
+router.post('/user/:id/delete', user_controller.deleteUser_p);
+
+// GET request to update user.
+router.get('/user/:id/update', user_controller.updateUser_g);
+
+// POST request to update user.
+router.post('/user/:id/update', user_controller.updateUser_p);
+
+// GET request for one user.
+router.get('/user/:id', user_controller.getUserById);
+
+// GET request for list of all users.
+router.get('/users', user_controller.getAllUsers);
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// POST ROUTES //
+// GET request for creating a post. NOTE This must come before route that displays post (uses id).
+router.get('/post/create', post_controller.createPost_g);
+
+// POST request for creating post.
+router.post('/post/create', post_controller.createPost_p);
+
+// GET request to delete post.
+router.get('/post/:id/delete', post_controller.deletePost_g);
+
+// POST request to delete post.
+router.post('/post/:id/delete', post_controller.deletePost_p);
+
+// GET request to update post.
+router.get('/post/:id/update', post_controller.updatePost_g);
+
+// POST request to update post.
+router.post('/post/:id/update', post_controller.updatePost_p);
+
+// GET request for one post.
+router.get('/post/:id', post_controller.getPostById);
+
+// GET request for list of all post.
+router.get('/posts', post_controller.getAllPosts);
 
 
 
